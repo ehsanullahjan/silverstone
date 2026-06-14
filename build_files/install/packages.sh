f3b179906f4ed8b96d51af7e1d8c616f5e584cc1
ehsanj <ehsanullahjan@gmail.com>
2026-06-14 03:00:39 -0400

    Revise packages installed on the main layer

--- build_files/install/packages.sh
+++ build_files/install/packages.sh
@@ -4,30 +4,22 @@ set -ouex pipefail
 
 # Install packages
 PACKAGES=(
-	bat
-	btop
-	carapace-bin
-	chezmoi
-	cowsay
-	eza
-	fastfetch
-	fd-find
-	fortune-mod
-	grc
-	iostat
-	iotop
-	ngrep
-	neovim
-	nicstat
-	nmap
-	nvtop
-	ripgrep
-	socat
-	tealdeer
-	tmux
-	trash-cli
-	xdg-terminal-exec
-	zoxide
-	zsh
+    bat
+    btop
+    eza
+    fastfetch
+    fd-find
+    grc
+    ngrep
+    nmap
+    nvtop
+    ripgrep
+    socat
+    tealdeer
+    tmux
+    vim
+    xdg-terminal-exec
+    zoxide
+    zsh
 )
 dnf -y install "${PACKAGES[@]}"
