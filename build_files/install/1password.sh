54e445573cdf88bc24faf5e9e132c20d72a5842e
ehsanj <ehsanullahjan@gmail.com>
2026-06-17 03:01:18 -0400

    Fix missing groups by employing systemd sysusers
    Groups added with `groupadd` when building an atomic OS turn out to be
    available only during build. This statisfies 1Password post-install
    scripts.
    
    In order to have the groups available when the system is booted,
    however, we need to employ systemd sysusers. With the right config
    persisted on the system, systemd automatically creates the groups at
    boot time.

--- build_files/install/1password.sh
+++ build_files/install/1password.sh
@@ -14,6 +14,14 @@ cat <<-EOF >/etc/yum.repos.d/1password.repo
 EOF
 dnf config-manager setopt 1password.enabled=0
 
+# Ensure 1pass post-install scripts can find the groups during build
 groupadd -r -g 31001 onepassword
 groupadd -r -g 31002 onepassword-mcp
+
+# Ensure systemd actually creates the groups on system boot
+cat <<-EOF >/usr/lib/sysusers.d/1password.conf
+	g onepassword 31001 -
+	g onepassword-mcp 31002 -
+EOF
+
 dnf -y --enable-repo=1password install 1password
