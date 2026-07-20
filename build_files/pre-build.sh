373b12db4aa643caa37d8a3b50e5ceabeb4448cb
ehsanj <ehsanullahjan@gmail.com>
2026-07-19 21:01:05 -0400

    Break config sync into pre- and post-build phases
    Syncing pre-build configs should be the first build activity. All config
    that an install script depends on belong in the pre-build configs.
    
    Syncing post-build configs should be the last build activity. Typically,
    unless a config is needed by a build script, it should be in post-build.
    
    This scheme ensures the configs we provide have the highest priority,
    while reducing our burden of dependency management to only to configs
    that should go into pre-build. Everything else that doesn't have an
    explicit need to go into pre- goes into post-build config.

--- build_files/pre-build.sh
+++ build_files/pre-build.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
+# Copy custom configs
+rsync -rvK /ctx/system_files/pre-build/ /
+
 # Enable dnf config-manager
 dnf -y install 'dnf5-command(config-manager)' rsync
-
-# Copy custom configs
-rsync -rvK /ctx/system_files/ /
