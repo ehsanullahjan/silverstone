359dbf0f85027dddb0b95b491f49d255befc5409
ehsanj <ehsanullahjan@gmail.com>
2026-07-25 18:14:22 -0400

    Leave fedora-cisco-openh264 alone
    The repo comes pre-enabled with the upstream image.

--- build_files/install/rpmfusion.sh
+++ build_files/install/rpmfusion.sh
@@ -2,8 +2,7 @@
 
 set -euxo pipefail
 
-# Enable rpmfusion and its dependencies
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+# Enable rpmfusion
 dnf -y install \
 	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
 	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
@@ -24,7 +23,6 @@ dnf -y install --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer
 dnf -y install libva-utils mesa-va-drivers-freeworld
 
 dnf config-manager setopt \
-	fedora-cisco-openh264.enabled=0 \
 	rpmfusion-free.enabled=0 \
 	rpmfusion-free-updates.enabled=0 \
 	rpmfusion-nonfree.enabled=0 \
