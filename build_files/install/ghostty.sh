18c0ccb95e9dc224018a7b4ad9729761aeba0b0b
ehsanj <ehsanullahjan@gmail.com>
2026-07-26 00:34:19 -0400

    Drop ghostty terminal

--- build_files/install/ghostty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-dnf copr -y enable scottames/ghostty
-dnf config-manager setopt copr:copr.fedorainfracloud.org:scottames:ghostty.enabled=0
-
-dnf -y --enable-repo=copr:copr.fedorainfracloud.org:scottames:ghostty install ghostty
