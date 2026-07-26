ab6143bf7667485be3f99eea6d75c38ab7765987
ehsanj <ehsanullahjan@gmail.com>
2026-07-26 02:55:39 -0400

    Refactor code to improve clarity

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,14 +7,15 @@ set -euxo pipefail
 /ctx/build_files/install/amd-hwe.sh
 /ctx/build_files/install/multimedia.sh
 
+/ctx/build_files/install/tailscale.sh
+/ctx/build_files/install/sysmon.sh
+
 /ctx/build_files/install/docker.sh
 /ctx/build_files/install/podman.sh
 /ctx/build_files/install/libvirt.sh
 
 /ctx/build_files/install/git.sh
 /ctx/build_files/install/packages.sh
-/ctx/build_files/install/sysmon.sh
-/ctx/build_files/install/tailscale.sh
 
 /ctx/build_files/install/1password.sh
 /ctx/build_files/install/flatpaks.sh
--- build_files/pre-build.sh
+++ build_files/pre-build.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-# Copy custom configs
-rsync -rvK /ctx/system_files/pre-build/ /
-
 # Enable dnf config-manager
 dnf -y install 'dnf5-command(config-manager)'
+
+# Copy custom configs
+rsync -rvK /ctx/system_files/pre-build/ /
 
 # Install terra repos
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
