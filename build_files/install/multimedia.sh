dc9e4b484e185c7afd6b302cfdf09e2f80da5f7b
ehsanj <ehsanullahjan@gmail.com>
2026-08-05 01:30:45 -0400

    Add openh264 codecs for native firefox

--- build_files/install/multimedia.sh
+++ build_files/install/multimedia.sh
@@ -7,7 +7,13 @@ dnf -y install \
 	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
 	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-# Enable add'l multimedia codecs (must list pkgs explicitly on silverblue base image)
+# Enable openh264 for native firefox
+dnf -y swap noopenh264 openh264 --allowerasing
+dnf -y install --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin \
+	mozilla-openh264 \
+	gstreamer1-plugin-openh264
+
+# Enable add'l multimedia codecs from rpmfusion (must list pkgs explicitly on silverblue base image)
 # >>> dnf -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf -y swap ffmpeg-free ffmpeg --allowerasing
 dnf -y install --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin \
